436ae1f8f08e378599933c5a870f04226040e1f5
Albert <amdeveloper2019@gmail.com>
2019-07-21 15:26:17 -0500

    ultimo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 import {BrowserModule} from '@angular/platform-browser';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
@@ -17,7 +18,8 @@ import {LandingComponent} from './components/landing/landing.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -8,8 +8,17 @@ import {ApiRestService} from '../../services/api-rest.service';
 })
 export class LandingComponent implements OnInit {
 
+  public dataEuro: number; 
+  public dataDollar: number;
+  public valueUSD: number;
+  public idleTime: number;
+  public reloj: any;
+
   constructor(public instService: ApiRestService) {
     this.consumeService();
+    this.idleTime = 0;
+    this.reloj = setInterval(() => { this.temporizador(); }, 600000); // 10 minute
+   // this.valueUSD = 1.34;
   }// constructor
 
   ngOnInit() {
@@ -17,10 +26,24 @@ export class LandingComponent implements OnInit {
 
   consumeService(){
      this.instService.getCurrency().subscribe(data => {
+     this.valueUSD = data.rates['USD'];
      console.log("monedas",data.rates['USD']);
-     
-    });
+     });
     
   }// consumeService
+
+  makeCalculate(){
+     this.dataDollar =  this.valueUSD * this.dataEuro;
+
+  }//makeCalculate
+
+  temporizador() {
+    this.idleTime = this.idleTime + 1;
+    // Cuando el temporizador alcanza los 10 minutos se llama de nuevo el servicio
+    if (this.idleTime >= 10) { // 10 minutes
+      this.consumeService();
+    }
+  }// temporizador
+
 
 }// LandingComponent
